286f49a3f9de1a96ef48453f02dff339ca94e408
thieugiatri4492 <hokage4492@gmail.com>
2024-01-06 07:53:41 +0000

    Change the region to us-east-2

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -29,7 +29,7 @@ terraform {
   backend "s3" {
     bucket = "vprofileaction4492"
     key    = "terraform.tfstate"
-    region = "us-east-1"
+    region = "us-east-2"
   }
 
   required_version = "~> 1.6.3"
